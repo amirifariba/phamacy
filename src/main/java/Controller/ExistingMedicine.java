38f77db5997e6511fdb8e3c50a08b5af7c032b50
fariba <f.amiri882@gmail.com>
2017-10-10 17:20:22 +0330

    ExistingMedicine class changed(many to one of medicine information)

--- src/main/java/Controller/ExistingMedicine.java
+++ src/main/java/Controller/ExistingMedicine.java
@@ -5,6 +5,43 @@ import javax.persistence.*;
 @Entity
 @Table(name = "existmedicine")
 public class ExistingMedicine {
-
+	@Id
+	@GeneratedValue
+	@Column(name = "ExistMedicineId")
+	private int ExistMedicineId;
+	@ManyToOne(cascade = CascadeType.ALL)
+	private MedicineInformation medicineInformation;
+	@Column(name = "Price")
+	private int Price;
+	@Column(name = "ExpireDate")
+	private int ExpireDate;
+	public int getExistMedicineId() {
+		return ExistMedicineId;
+	}
+	public void setExistMedicineId(int existMedicineId) {
+		ExistMedicineId = existMedicineId;
+	}
+	public MedicineInformation getMedicineInformation() {
+		return medicineInformation;
+	}
+	public void setMedicineInformation(MedicineInformation medicineInformation) {
+		this.medicineInformation = medicineInformation;
+	}
+	public int getPrice() {
+		return Price;
+	}
+	public void setPrice(int price) {
+		Price = price;
+	}
+	public int getExpireDate() {
+		return ExpireDate;
+	}
+	public void setExpireDate(int expireDate) {
+		ExpireDate = expireDate;
+	}
+	
+	
+	
+	
 }
 
